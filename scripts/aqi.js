3eab34d69be4e416aa48746236fb137348235e51
Ganesh Raghav <ganeshraghav07@gmail.com>
2025-05-25 11:46:51 +0530

    Update aqi.js

--- scripts/aqi.js
+++ scripts/aqi.js
@@ -6,16 +6,17 @@ if (!aqiCity) {
   window.location.href = "../index.html";
 }
 async function getAQI() {
-  const res = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${aqiCity}&aqi=yes`);
+  const res = await fetch(`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`);
   const data = await res.json();
   const air = data.current.air_quality;
   document.getElementById("aqiData").innerHTML = `
-    <p>CO: ${air.co}</p>
-    <p>NO2: ${air.no2}</p>
-    <p>O3: ${air.o3}</p>
-    <p>PM2.5: ${air.pm2_5}</p>
-    <p>PM10: ${air.pm10}</p>
-    <p>US EPA Index: ${air["us-epa-index"]}</p>
-  `;
-}
+      <p>CO: ${air.co} μg/m³</p>
+      <p>NO: ${air.no} μg/m³</p>
+      <p>NO2: ${air.no2} μg/m³</p>
+      <p>O3: ${air.o3} μg/m³</p>
+      <p>SO2: ${air.so2} μg/m³</p>
+      <p>PM2.5: ${air.pm2_5} μg/m³</p>
+      <p>PM10: ${air.pm10} μg/m³</p>
+      <p>AQI Index: ${aqiIndex} (1=Good, 5=Very Poor)</p>
+    `;}
 getAQI();
