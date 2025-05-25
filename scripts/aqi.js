97c687a3b114250da4116fe71eb757b21947da54
Ganesh Raghav <ganeshraghav07@gmail.com>
2025-05-25 11:16:22 +0530

    Update aqi.js

--- scripts/aqi.js
+++ scripts/aqi.js
@@ -1,5 +1,5 @@
 
-const apiKey = "c350ab9eac2b4f5289881429251205";
+const apiKey = "b376483ccd681e040f9c573d3eae2f2f";
 const aqiCity = localStorage.getItem("city");
 if (!aqiCity) {
   alert("Please search a city from home page first.");
