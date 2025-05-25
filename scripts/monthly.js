5edf5961be971064dbed0acd9ece846d255d265f
Ganesh Raghav <ganeshraghav07@gmail.com>
2025-05-25 11:17:03 +0530

    Update monthly.js

--- scripts/monthly.js
+++ scripts/monthly.js
@@ -1,5 +1,5 @@
 
-const apiKey = "c350ab9eac2b4f5289881429251205";
+const apiKey = "b376483ccd681e040f9c573d3eae2f2f";
 const monthlyCity = localStorage.getItem("city");
 if (!monthlyCity) {
   alert("Please search a city from home page first.");
